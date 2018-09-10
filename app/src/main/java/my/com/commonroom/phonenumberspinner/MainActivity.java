677ede2f935ae69a40542027ddc33fc2f5eaaf44
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-09-10 21:56:06 +0800

    first commit

---
+++ app/src/main/java/my/com/commonroom/phonenumberspinner/MainActivity.java
@@ -0,0 +1,66 @@
+package my.com.commonroom.phonenumberspinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // Define mSpinnerLabel for the label (the spinner item that the user chooses).
+    private String mSpinnerLabel = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+
+    public void showText(View view) {
+        EditText editText =  findViewById(R.id.editText_main);
+        if (editText != null) {
+            // Assign to showString both the entered string and mSpinnerLabel.
+            String showString = (editText.getText().toString() + " - " + mSpinnerLabel);
+            // Assign to phoneNumberResult the view for text_phonelabel to prepare to show it.
+            TextView phoneNumberResult = findViewById(R.id.text_phonelabel);
+            // Show the showString in the phoneNumberResult.
+            if (phoneNumberResult != null) phoneNumberResult.setText(showString);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        mSpinnerLabel = adapterView.getItemAtPosition(i).toString();
+        showText(view);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.d(TAG, getString(R.string.nothing_selected));
+    }
+}
