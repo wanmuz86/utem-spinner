64826441d8748c264e1e880728543c42724f5715
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-09-13 14:34:59 +0800

    first commit

---
+++ app/src/androidTest/java/my/com/commonroom/phonenumberspinner/SpinnerSelectionTest.java
@@ -0,0 +1,33 @@
+package my.com.commonroom.phonenumberspinner;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class SpinnerSelectionTest {
+
+
+}
+
+
